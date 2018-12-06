bc7a3530dd64aafe8139c087edc94cc097062bb5
grupoed1 <45070214+grupoed1@users.noreply.github.com>
2018-12-05 23:12:43 -0300

    leitura de arquivo

--- main.c
+++ main.c
@@ -1,77 +1,79 @@
 #include "via_2.h"
+#include "via_1.h"
 #include "estacionamento.h"
 #include "engarrafamento.h"
 
 int main(){
 	
 	int ciclo = 0;
+	Via_1* via1;
 	Via_2* faixa_1;
 	Via_2* faixa_2;
 	Estacionamento* ES;
-	Engarrafamento* EN;
-	Carro novo;
+	Engarrafamento* EN1;
+	Engarrafamento* EN2;
+	Carro novo, saidav1[2], saidav2_1, saidav2_2, saidaes, saidaen1, saidaen2;
 
-	char resp, resp2;
+	FILE* veiculos;
 
+	char carro_atual[23];
+	char buffer[4];
+	int i = 2, p = 0;
+	char t = '&';
 
-	do{	
 		ciclo++;
-		Desenfileirar(faixa_1, 1);
-		Desenfileirar(faixa_2, 2);
-		printf("Adicionar carro?\n");
-		scanf(" %c", &resp);
+		saidav2_1 = Desenfileirar(faixa_1, 1);
+		saidav2_2 = Desenfileirar(faixa_2, 2);
+		saidav1 = CicloVia_1(via1);
+		saidaes = ControleSaidaES(ES);
+		saidaen1 = sairEngarrafamento(EN1);
+		saidaen2 = sairEngarrafamento(EN2);
+		atualizarestacionamento(ES);
 
-		if (resp == 's'){
-			printf("Digite o tipo\n");
-			scanf(" %c", &novo.tipov);	
-			printf("Digite o numero\n");
-			scanf("%d", &novo.nveiculo);	
-			printf("Digite a origem\n");
-			scanf("%d", &novo.origem);	
-			novo.instante = ciclo;
-			printf("Digite o destino final\n");
-			scanf("%d", &novo.dfinal);
-			printf("Estacionamento?\n");
-			scanf(" %c", &novo.estacionamento);
-			printf("Digite o tempo no estacionamento\n");
-			scanf("%d", &novo.testacionamento);
+		veiculos = fopen("veiculos.txt", "r");
+		fgets(carro_atual, 23, (FILE*) veiculos);
 
-			if (novo.origem == 1){
-				if (novo.estacionamento == 'S'){
-					if (ES->topo != Z-1)
-						empilhar(ES, novo);
-					else{
-						if (novo.dfinal == 2){
-							if (Qtecarros(faixa_1) <= Qtecarros(faixa_2))
-								Enfileirar(faixa_1, novo, 1);
-							else
-								Enfileirar(faixa_2, novo, 2);
-						}
-						else{
-
-						}
-					}
-				}
-				else{
-					if (novo.dfinal == 2){
-						if (Qtecarros(faixa_1) <= Qtecarros(faixa_2))
-							Enfileirar(faixa_1, novo, 1);
-						else
-							Enfileirar(faixa_2, novo, 2);
-					}
-					else{
-				
-					}
-				}
-			}
-			else{
-
-			}
-		}
-
-		printf("Continuar?\n");
-		scanf(" %c", &resp);
-	}while (resp == 's');
+		novo.tipov = carro_atual[0];
+		do{
+			t = carro_atual[i];
+			if (t != ' ')
+				buffer[i - 2] = t;
+			else
+				break;
+			i++;
+		}while (i < 6);
+		novo.nveiculo = int(buffer);
+		strcpy(buffer, '    ');
+		i++;
+		novo.origem = int(carro_atual[i]);
+		i += 2;
+		do{
+			t = carro_atual[i];
+			if (t != ' ')
+				buffer[p] = t;
+			else
+				break;
+			i++;
+			p++;
+		}while (p < 4);
+		novo.instante = int(buffer);
+		i++;
+		novo.dfinal = int(carro_atual[i]);
+		i += 2;
+		novo.estacionamento = carro_atual[i];
+		i += 2;
+		strcpy(buffer, '    ');
+		p = 0;
+		do{
+			t = carro_atual[i];
+			if (t != ' ')
+				buffer[p] = t;
+			else
+				break;
+			i++;
+			p++;
+		}while (p < 4);
+		novo.testacionamento = int(buffer);
 
 	
 	return 0;
