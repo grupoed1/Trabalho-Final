81822919429111bf8a5fde07cb8bfa4aec2aab95
grupoed1 <45070214+grupoed1@users.noreply.github.com>
2018-12-07 13:31:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include "via_2.h"
 #include "via_1.h"
 #include "estacionamento.h"
 #include "engarrafamento.h"
@@ -6,12 +5,12 @@
 int main(){
 	
 	int ciclo = 0;
-	Via_1* via1;
-	Via_2* faixa_1;
-	Via_2* faixa_2;
-	Estacionamento* ES;
-	Engarrafamento* EN1;
-	Engarrafamento* EN2;
+	Via_1* via1 = (Via_1*) malloc(sizeof(Via_1));
+	Via_2* faixa_1 = (Via_2*) malloc(sizeof(Via_2));
+	Via_2* faixa_2 = (Via_2*) malloc(sizeof(Via_2));
+	Estacionamento* ES = (Estacionamento*) malloc(sizeof(Estacionamento));
+	Engarrafamento* EN1 = (Engarrafamento*) malloc(sizeof(Engarrafamento));
+	Engarrafamento* EN2 = (Engarrafamento*) malloc(sizeof(Engarrafamento));;
 	Carro novo, saidav1[2], saidav2_1, saidav2_2, saidaes, saidaen1, saidaen2;
 
 	FILE* veiculos;
@@ -21,14 +20,22 @@ int main(){
 	int i = 2, p = 0;
 	char t = '&';
 
+	inicializarVia_1(via1);
+	inicializar(faixa_1);
+	inicializar(faixa_2);
+	inicializarES(ES);
+	iniciarEngarrafamento(EN1);
+	iniciarEngarrafamento(EN2);
+
 		ciclo++;
 		saidav2_1 = Desenfileirar(faixa_1, 1);
 		saidav2_2 = Desenfileirar(faixa_2, 2);
-		saidav1 = CicloVia_1(via1);
+		saidav1[0] = CicloVia_1(via1)[0];
+		saidav1[1] = CicloVia_1(via1)[1];
 		saidaes = ControleSaidaES(ES);
 		saidaen1 = sairEngarrafamento(EN1);
 		saidaen2 = sairEngarrafamento(EN2);
-		atualizarestacionamento(ES);
+		atualizarEstacionamento(ES);
 
 		veiculos = fopen("veiculos.txt", "r");
 		fgets(carro_atual, 23, (FILE*) veiculos);
@@ -42,10 +49,10 @@ int main(){
 				break;
 			i++;
 		}while (i < 6);
-		novo.nveiculo = int(buffer);
-		strcpy(buffer, '    ');
+		novo.nveiculo = atoi(buffer);
+		strcpy(buffer, "   ");
 		i++;
-		novo.origem = int(carro_atual[i]);
+		novo.origem = atoi(&carro_atual[i]);
 		i += 2;
 		do{
 			t = carro_atual[i];
@@ -56,13 +63,14 @@ int main(){
 			i++;
 			p++;
 		}while (p < 4);
-		novo.instante = int(buffer);
+		buffer[p] = '\0';
+		novo.instante = atoi(buffer);
 		i++;
-		novo.dfinal = int(carro_atual[i]);
+		novo.dfinal = atoi(&carro_atual[i]);
 		i += 2;
 		novo.estacionamento = carro_atual[i];
 		i += 2;
-		strcpy(buffer, '    ');
+		strcpy(buffer, "   ");
 		p = 0;
 		do{
 			t = carro_atual[i];
@@ -72,8 +80,9 @@ int main(){
 				break;
 			i++;
 			p++;
-		}while (p < 4);
-		novo.testacionamento = int(buffer);
+		}while (p < 4 && i < strlen(carro_atual));
+		buffer[p] = '\0';
+		novo.testacionamento = atoi(buffer);
 
 	
 	return 0;
