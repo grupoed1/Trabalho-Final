515aaf08d5403862a6613ca2637d0338ae169246
grupoed1 <45070214+grupoed1@users.noreply.github.com>
2018-12-11 19:34:53 -0300

    Update image.py

--- image.py
+++ image.py
@@ -2,11 +2,27 @@
 
 import Tkinter as tk
 from PIL import ImageTk, Image
+from tkFileDialog import askopenfilename
 import subprocess
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
+
+def callback(n):
+	name = askopenfilename()
+	file = open(name, 'r')
+	x = file.read()
+	if len(x) > 1:
+		if n == 1:
+			buff = 'veiculos.txt'
+		elif n == 2:
+			buff = 'eventos.txt'
+		file_d = open(buff, 'r+')
+		file_d.truncate(0)
+		file_d = open(buff, 'w')
+		file_d.write(x)
+	file.close()
 
 def answer():
 	subprocess.call("./main", shell = True)
@@ -23,5 +39,9 @@ photoImg = ImageTk.PhotoImage(img)
 canvas.create_image(318, 178, image = photoImg)
 initiate_button = tk.Button(window, text = "Iniciar", command = answer, anchor = 'w', width = 10, activebackground = "#33B5E5")
 initiate_button_window = canvas.create_window(318, 315, anchor = 'center', window = initiate_button)
+veiculos_button = tk.Button(window, text = "Veiculos", command = lambda: callback(n = 1), anchor = 'w', width = 10, activebackground = "#33B5E5")
+veiculos_button_window = canvas.create_window(158, 315, anchor = 'center', window = veiculos_button)
+eventos_button = tk.Button(window, text = "Eventos", command = lambda: callback(n = 2), anchor = 'w', width = 10, activebackground = "#33B5E5")
+eventos_button_window = canvas.create_window(478, 315, anchor = 'center', window = eventos_button)
 
 window.mainloop()
